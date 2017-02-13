1b2037430a76ef488684e7f419ddf95b4faeda5e
m1ksu <mikael.stenman112@gmail.com>
2017-02-13 06:56:49 +0200

    Created repo

---
+++ README.md
@@ -0,0 +1,2 @@
+# Vertigo - VR on the Go
+### A website for a small company named Vertigo.
---
+++ script.js
@@ -0,0 +1,3 @@
+$(document).ready(function () {
+    $('.header').parallax({imageSrc: 'img/bg1.jpg'});
+})
