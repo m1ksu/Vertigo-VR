1e5024dba82f45527bc07031ed3b3fb337de4439
m1ksu <mikael.stenman112@gmail.com>
2017-02-23 22:03:03 +0200

    Added JS for the drop down menu.

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 $(document).ready(function () {
     $('.header').parallax({imageSrc: 'img/bg1.jpg'});
+
+    $('.dropdown').slideUp(0);
+
+    var dropdownToggled = false;
+    $('#first-link').on('click', function() {
+        if (!dropdownToggled) {
+            $('.dropdown').slideDown();
+            dropdownToggled = true;
+        } else {
+            $('.dropdown').slideUp();
+            dropdownToggled = false;
+        }
+    });
 })
