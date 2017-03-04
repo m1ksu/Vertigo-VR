72bc41a4ac3ea7af9ea0f694e7cfe52d5d71c1ec
m1ksu <mikael.stenman112@gmail.com>
2017-03-04 16:28:19 +0200

    asdsad

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
     $('.dropdown').slideUp(0);
 
     var dropdownToggled = false;
-    $('#first-link').on('click', function() {
+    $('.first-link').on('click', function() {
         if (!dropdownToggled && $(window).width() < 760) {
             $('.dropdown').slideDown();
             dropdownToggled = true;
@@ -14,6 +14,31 @@ $(document).ready(function () {
             dropdownToggled = false;
         }
     });
+    navActive(null);
+    var page = window.location.pathname;
+    switch (page) {
+        case "/":
+            navActive('home');
+            break;
+
+        case "/portfolio.html":
+            navActive('portfolio');
+            break;
+
+        case "/services.html":
+            navActive('services');
+            break;
+
+        case "/contact.html":
+            navActive('contact');
+            break;
+    }
+    console.log(page);
+
+    function navActive(page) {
+        $('.navigator *').removeClass("nav-active");
+        $("#" + page).addClass("nav-active");
+    }
 })
 
 $(window).resize(function() {
